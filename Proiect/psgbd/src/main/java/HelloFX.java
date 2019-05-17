023529b31627a82ebafba2184d636099b93bcc69
Emilian Radu <emiradu98@icloud.com>
2019-05-17 19:05:25 +0300

    Added Proj

--- Proiect/psgbd/src/main/java/HelloFX.java
+++ Proiect/psgbd/src/main/java/HelloFX.java
@@ -10,8 +10,9 @@ public class HelloFX extends Application {
     public void start(Stage stage) {
         String javaVersion = System.getProperty("java.version");
         String javafxVersion = System.getProperty("javafx.version");
-        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+        Label l = new Label("Hello, JavaFX " +", running on Java ");
         Scene scene = new Scene(new StackPane(l), 640, 480);
+        stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
     }
