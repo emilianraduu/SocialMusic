8a9e9d200cc0363b2e920fae64d5b24069f6c2ba
Emilian Radu <emiradu98@icloud.com>
2019-05-22 12:35:21 +0300

    Finished

--- Proiect/psgbd/src/main/java/HelloFX.java
+++ Proiect/psgbd/src/main/java/HelloFX.java
@@ -1,24 +1,12 @@
+import controller.ConnectionController;
 import javafx.application.Application;
 
-import static javafx.application.Application.launch;
-
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.PasswordField;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.text.Text;
-import javafx.scene.control.TextField;
 import javafx.stage.Stage;
-import view.MainFrame;
 
 public class HelloFX extends Application {
     @Override
     public void start(Stage stage) {
-
+        ConnectionController c=new ConnectionController();
     }
 
     public static void main(String args[]) {
